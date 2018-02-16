67bf644837b27c7d163a9eb34c096947de881f05
Johannes Iber <johannes.iber@tugraz.at>
2018-02-16 13:55:39 +0100

    fixed sed: RE error: illegal byte sequence for macos

--- dict.cc.sh
+++ dict.cc.sh
@@ -16,7 +16,7 @@ function dict {
 
     local sanitized=$(echo "$input" | tr ' ' '+')
     local text=$(lynx -dump http://"$lang"pocket.dict.cc/?s="$sanitized" | \
-                  sed '/sec/q' | tail -n +6 )
+                  LC_ALL=C sed '/sec/q' | tail -n +6 )
     
     local nLines=$(echo "$text" | wc -l ) 
     
